7663bf4d9c66ec317f902e1d6cc27b3e7c367b65
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2013-12-10 16:40:04 +0100

    First naive implementation

--- demo.py
+++ demo.py
@@ -1,7 +1,13 @@
+from __future__ import unicode_literals
 import parla
+import codecs
 
 def main():
-    pass
+    result = parla.parse("./assets/quixote.txt")
+    with codecs.open("./result.txt", "wb", "utf-8") as f:
+        for r in result:
+            f.write(u"{0}: {1}\n".format(r[0], r[1]))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
---
+++ parla/api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import codecs
+import operator
+
+
+def parse(path):
+    if not os.path.exists(path):
+        raise IOError("Cannot access the file {0}".format(path))
+
+    words = {}
+    # This allows us to read line by line without buffering the
+    # entire file
+    with codecs.open(path, "r", "utf-8-sig", "utf-8") as f:
+        for line in f:
+            for w in line.split():
+                w = w.strip()  # Remove whitespace
+                w = w.strip(",.:;-'\"?¿!¡»«")
+                w = w.lower()
+                words[w] = words.get(w, 0) + 1
+    
+    return sorted(words.iteritems(), key=operator.itemgetter(1), reverse=True)
