bd52be972a9a95d7cdb50802b52886da9c1a32a5
Ignacio Contreras Pinilla <ignacio@ignacio.cat>
2013-12-15 18:00:17 +0100

    Minor refactor

--- parla/api.py
+++ parla/api.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 class Zipf(object):
 
     def __init__(self, path):
-        self.result = []
+        self.words = []
         self.path = path
-        self.result = Zipf.parse(self.path)
+        self.words = Zipf.parse(self.path)
 
     @staticmethod
     def parse(path):
@@ -46,22 +46,22 @@ class Zipf(object):
         return num_words
 
     def summary(self):
-        total_words = sum(r[1] for r in self.result)
-        total_unique_words = len(self.result)
+        total_words = sum(r[1] for r in self.words)
+        total_unique_words = len(self.words)
         print("Number of words: {0}".format(total_words))
         print("Number of unique words: {0}".format(total_unique_words))
         print("Top ten:")
         for i in range(10):
-            w = self.result[i]
+            w = self.words[i]
             print("{0} - {1} - {2} - {3}%".format(i, w[0], w[1], 100.0*float(w[1])/float(total_words)))
-        print("\nNumber of words needed to understand 50% of the text: {0}".format(Zipf.words_needed(self.result, 50.0, total_words)))
-        print("\nNumber of words needed to understand 60% of the text: {0}".format(Zipf.words_needed(self.result, 60.0, total_words)))
-        print("\nNumber of words needed to understand 80% of the text: {0}".format(Zipf.words_needed(self.result, 80.0, total_words)))
+        print("\nNumber of words needed to understand 50% of the text: {0}".format(Zipf.words_needed(self.words, 50.0, total_words)))
+        print("\nNumber of words needed to understand 60% of the text: {0}".format(Zipf.words_needed(self.words, 60.0, total_words)))
+        print("\nNumber of words needed to understand 80% of the text: {0}".format(Zipf.words_needed(self.words, 80.0, total_words)))
     
 
     def plot(self):
         x = range(100)
-        y = [word[1] for word in self.result[:100]]
+        y = [word[1] for word in self.words[:100]]
         plt.xkcd()
         plt.plot(x, y)
 
