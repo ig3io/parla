e1ef466cf9c846544b39420ed4f03c36f32ff2e8
Ignacio Contreras Pinilla <ignacio@ignacio.cat>
2013-12-17 07:37:31 +0100

    Fix demo.py and add real result.png

--- demo.py
+++ demo.py
@@ -9,12 +9,12 @@ import matplotlib.pyplot as plt
 
 def main():
     p = parla.Zipf("./assets/quixote.txt")
-    #with codecs.open("./result.txt", "wb", "utf-8") as f:
-    #    for r in p.words:
-    #        f.write(u"{0}: {1}\n".format(r[0], r[1]))
+    with codecs.open("./result.txt", "wb", "utf-8") as f:
+        for r in p.words:
+            f.write(u"{0}: {1}\n".format(r[0], r[1]))
     p.summary()
     p.plot()
-    p.show()
+    #p.show()
     p.save("result.png")
 
 if __name__ == "__main__":
